37ffe7daa683e145dbd0f94579ae252b83858a24
asrcpq <asrcpq@outlook.com>
2020-12-26 14:59:29 +0000

    separated plugin

--- myjump.zsh
+++ myjump.zsh
@@ -38,12 +38,3 @@ myjump() {
 	done
 	return 1
 }
-
-mkdir -p $HOME/.local/share/myjump
-MYJUMP_FILE=$HOME/.local/share/myjump/data
-while read -r line; do
-	MYJUMP_DATA+=($line)
-done <$MYJUMP_FILE
-autoload -U add-zsh-hook
-add-zsh-hook precmd myjump_precmd
-zle -N myjump_precmd
